d250f046c9588a245ce6f19d2477207a4d22c102
Patrick O'Neill <patrick.oneill46@gmail.com>
2017-01-11 18:48:41 +0000

    Bounding drag move to the workspace

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
 import { ResizeEvent } from 'angular2-resizable';
 
+let initialStyle = {
+  height: 300,
+  width: 200,
+  top: 100,
+  left: 100
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,9 +15,20 @@ import { ResizeEvent } from 'angular2-resizable';
 })
 export class AppComponent {
   title = 'app works!';
-
-  style = {};
+  style = {
+    left: `${initialStyle.left}px`,
+    top: `${initialStyle.top}px`,
+    width: `${initialStyle.width}px`,
+    height: `${initialStyle.height}px`
+  };
+  styleIntegers = {
+    left: initialStyle.left,
+    top: initialStyle.top,
+    width: initialStyle.width,
+    height: initialStyle.height
+  };
   dragStart = {};
+  workspaceRect;
 
   validate(event: ResizeEvent): boolean {
     const MIN_DIMENSIONS_PX: number = 50;
@@ -23,6 +41,8 @@ export class AppComponent {
   onResizeEnd(event: ResizeEvent): void {
     console.log(event, 'resizeEnd');
 
+    Object.assign(this.styleIntegers, event.rectangle);
+
     this.style = {
       left: `${event.rectangle.left}px`,
       top: `${event.rectangle.top - 60}px`,
@@ -33,9 +53,31 @@ export class AppComponent {
 
   onDrag(event) {
 
+    let previousStyle: any = Object.assign({}, this.styleIntegers);
+    let newStyle: any = {};
+
+    if (event.x + previousStyle.width > window.innerWidth) {
+      newStyle.left = window.innerWidth - previousStyle.width;
+    } else if (event.x < 0) {
+      newStyle.left = 0;
+    } else {
+      newStyle.left = event.x;
+    }
+
+    if (event.y + previousStyle.height > window.innerHeight) {
+      newStyle.top = window.innerHeight - previousStyle.height;
+    } else if (event.y < 60) {
+      newStyle.top = 60;
+    } else {
+      newStyle.top = event.y;
+    }
+
+    Object.assign(this.styleIntegers, newStyle);
     this.style = {
-      left: `${event.x}px`,
-      top: `${event.y - 60}px`,
+      left: `${newStyle.left}px`,
+      top: `${newStyle.top - 60}px`,
+      width: `${previousStyle.width}px`,
+      height: `${previousStyle.height}px`
     }
   }
 
