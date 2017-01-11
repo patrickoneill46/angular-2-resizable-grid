6f5e160325893dd25da0cecd24a224b5b1f7bd7f
Patrick O'Neill <patrick.oneill46@gmail.com>
2017-01-11 16:55:29 +0000

    Adding resizable directive to workspacae item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ResizeEvent } from 'angular2-resizable';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  style = {};
+
+  validate(event: ResizeEvent): boolean {
+    const MIN_DIMENSIONS_PX: number = 50;
+    if (event.rectangle.top < 60 || event.rectangle.left < 0 || event.rectangle.right > window.innerWidth || event.rectangle.bottom > window.innerHeight) {
+      return false;
+    }
+    return true;
+  }
+
+  onResizeEnd(event: ResizeEvent): void {
+    console.log(event, 'resizeEnd');
+
+    this.style = {
+      left: `${event.rectangle.left}px`,
+      top: `${event.rectangle.top - 60}px`,
+      width: `${event.rectangle.width}px`,
+      height: `${event.rectangle.height}px`
+    };
+  }
 }
