905f923c41cc70e30b385c8636e9aa08dc5715eb
Patrick O'Neill <patrick.oneill46@gmail.com>
2017-01-27 09:53:39 +0000

    Adding workspace methods

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 import { WorkspaceService } from '../workspace.service';
+
 
 @Component({
   selector: 'header',
@@ -10,25 +12,53 @@ import { WorkspaceService } from '../workspace.service';
 export class HeaderComponent implements OnInit {
 
   workspaces: any[];
+  creatingWorkspace: boolean;
+  newWorkspaceName: string;
 
   constructor(private workspaceService: WorkspaceService) { }
 
+  @HostListener('window:keyup', ['$event'])
+  onKeyup(event): void {
+
+    if (this.creatingWorkspace) {
+
+      switch (event.keyCode) {
+
+        case 27:
+          this.hideNewWorkspaceForm();
+
+        case 13:
+          this.createNewWorkspace();
+      }
+    }
+  }
+
   ngOnInit() {
     this.workspaces = [];
+    this.newWorkspaceName = '';
     this.workspaceService.getWorkspaces().subscribe(workspaces => this.updateWorkspaceSelector(workspaces))
   }
 
-  resetWorkspace() {
+  resetWorkspace(): void {
 
     this.workspaceService.resetWorkspace();
     location.reload();
   }
 
-  showWorkspace(workspaceId) {
+  showWorkspace(workspaceId): void {
     this.workspaceService.showWorkspace(workspaceId);
   }
 
-  private updateWorkspaceSelector(workspaces) {
+  showNewWorkspaceForm(): void {
+    this.creatingWorkspace = true;
+  }
+
+  hideNewWorkspaceForm(): void {
+    this.creatingWorkspace = false;
+    this.newWorkspaceName = '';
+  }
+
+  private updateWorkspaceSelector(workspaces): void {
 
     Object.keys(workspaces).forEach(workspaceKey => {
       this.workspaces.push({
@@ -38,5 +68,7 @@ export class HeaderComponent implements OnInit {
     });
   }
 
-
+  private createNewWorkspace(): void {
+    console.log('creating new workspace');
+  }
 }
